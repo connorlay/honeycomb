cf399b626d5de9140b14673b0d178be466ac03e2
Connor Lay <connor.lay@me.com>
2016-12-30 13:35:50 -0500

    Extract all json schemas from an apib

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,25 @@
 module Main where
 
-import           Data.ByteString.Lazy as B (readFile)
-import           Data.Json            (decodeJson)
-import           Network.Apiary       (parseApib)
+import           Data.Aeson              (Value)
+import           Data.ApiElement
+import           Data.ByteString.Lazy    as B (readFile)
+import           Data.Json               (decodeJson)
+import           Network.Apiary          (parseApib)
+import           Search.DepthFirstSearch
 
 main :: IO ()
 main = do
   resp <- parseApib =<< B.readFile "simple.apib"
-  print . decodeJson $ resp
+  case decodeJson resp of
+    Left msg ->
+      print msg
+    Right v ->
+      print . parseJsonSchemas . findJsonSchemas $ v
+
+findJsonSchemas :: Value -> [Value]
+findJsonSchemas x =
+  traverseAst x isASchema
+
+parseJsonSchemas :: [Value] -> [Maybe Value]
+parseJsonSchemas xs =
+  map (jsonSchema) xs
