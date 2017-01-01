4824403b9a0e63e730aebed8acb9be6beca0c820
Connor Lay <connor.lay@me.com>
2017-01-01 17:40:52 -0500

    Finish example lombok code generation

--- src/Language/Java/Lombok.hs
+++ src/Language/Java/Lombok.hs
@@ -5,18 +5,41 @@ import Data.Aeson (Value)
 
 type Schema = Value
 
-generateAst :: Schema -> CompilationUnit
-generateAst schema =
-  CompilationUnit Nothing [] [
-    ClassTypeDecl (ClassDecl [Public] (Ident "Complex") [] Nothing [] (
-      ClassBody
-        [ MemberDecl (FieldDecl
-            [Private]
-            (RefType (ClassRefType (ClassType [(Ident "String", [])])))
-            [VarDecl (VarId (Ident "id")) Nothing])
-        , MemberDecl (FieldDecl
-            [Private]
-            (RefType (ClassRefType (ClassType [
-              (Ident "List", [ActualType (ClassRefType (ClassType [(Ident "Widget", [])]))])])))
-            [VarDecl (VarId (Ident "widgets")) Nothing])
-        ]))]
+generateAst :: CompilationUnit
+generateAst =
+  CompilationUnit Nothing
+    [ ImportDecl False (Name [
+        (Ident "java")
+      , (Ident "util")
+      , (Ident "List")
+      ]) False
+    , ImportDecl False (Name [
+        (Ident "lombok")
+      , (Ident "Data")
+      ]) False
+    ]
+    [ ClassTypeDecl (ClassDecl
+      [Annotation (MarkerAnnotation {annName = Name [Ident "Data"]}), Public]
+      (Ident "Complex") [] Nothing [] (
+        ClassBody
+          [ MemberDecl (FieldDecl
+              [Private]
+              (RefType (ClassRefType (ClassType [(Ident "String", [])])))
+              [VarDecl (VarId (Ident "id")) Nothing])
+          , MemberDecl (FieldDecl
+              [Private]
+              (RefType (ClassRefType (ClassType [
+                (Ident "List", [ActualType (ClassRefType (ClassType [(Ident "Widget", [])]))])])))
+              [VarDecl (VarId (Ident "widgets")) Nothing])
+          , MemberDecl (MemberClassDecl (ClassDecl
+              [Annotation (MarkerAnnotation {annName = Name [Ident "Data"]}), Public, Static]
+              (Ident "Widget") [] Nothing [] (
+                ClassBody
+                  [ MemberDecl (FieldDecl
+                    [Public]
+                    (RefType (ClassRefType (ClassType [(Ident "String",[])])))
+                    [VarDecl (VarId (Ident "id")) Nothing])])))
+          ]
+        )
+      )
+    ]
