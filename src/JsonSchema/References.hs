04399322062b7db3d065e96b692280e90174068f
Connor Lay <conner.lay@nike.com>
2017-01-31 15:19:57 -0800

    Make Schema expandable

---
+++ src/JsonSchema/References.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module JsonSchema.References where
+
+import           Search.DepthFirstSearch
+import           Data.JsonSchema.Draft4.Schema (Schema (..))
+import           Data.Validator.Draft4.Any     (TypeValidator (..))
+import           Data.Validator.Draft4.Array   (Items (..))
+import           Data.Maybe (fromMaybe, maybeToList)
+import           Data.HashMap.Lazy             (HashMap (..), empty, toList)
+
+instance Expandable Schema where
+  expand schema =
+    case _schemaType schema of
+      Just (TypeValidatorString "object") ->
+        map snd . toList . fromMaybe empty . _schemaProperties $ schema
+
+      Just (TypeValidatorString "array") ->
+        maybeToList . toSubschema $ schema
+
+      _ ->
+        []
+
+toSubschema :: Schema -> Maybe Schema
+toSubschema schema =
+  case _schemaItems schema of
+    Just (ItemsObject subschema) ->
+      Just subschema
+
+    _ ->
+      Nothing
+
+
