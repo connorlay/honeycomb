0635a24fb4f86bbb819c356631d866fd0d936d1b
Connor Lay <connor.lay@me.com>
2017-01-31 21:48:28 -0800

    Extract ref strings from schema

--- src/JsonSchema/References.hs
+++ src/JsonSchema/References.hs
@@ -6,8 +6,9 @@ import           Search.DepthFirstSearch
 import           Data.JsonSchema.Draft4.Schema (Schema (..))
 import           Data.Validator.Draft4.Any     (TypeValidator (..))
 import           Data.Validator.Draft4.Array   (Items (..))
-import           Data.Maybe (fromMaybe, maybeToList)
+import           Data.Maybe                    (catMaybes, fromMaybe, maybeToList, isJust)
 import           Data.HashMap.Lazy             (HashMap (..), empty, toList)
+import           Data.Text                     (Text)
 
 instance Expandable Schema where
   expand schema =
@@ -30,9 +31,9 @@ toSubschema schema =
     _ ->
       Nothing
 
-collectRefs :: Schema -> [String]
+collectRefs :: Schema -> [Text]
 collectRefs schema =
-  traverseAst schema isARef
+  catMaybes . map _schemaRef . traverseAst schema $ isARef
   where
     isARef :: Schema -> Bool
     isARef = isJust . _schemaRef
